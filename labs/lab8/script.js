bf67be9556ef865381038ad4c5f0aa5aa3d8b098
Jordan Gilbert <jordan.gilbert@berkeley.edu>
2022-11-13 20:35:53 -0800

    finished lab8

---
+++ labs/lab8/script.js
@@ -0,0 +1,6 @@
+function toggleTheme() {
+    let body = document.body;
+    body.classList.toggle("dark-mode");
+}
+let buttonVariable = document.getElementById("toggleButton")
+buttonVariable.onclick = toggleTheme;
